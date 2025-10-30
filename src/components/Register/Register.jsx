6c41a1c6c98c03353aed580aad52f7dc2cd0ca2a
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-10-30 11:58:04 +0600

    toggle Show password and Accept terms and conditions

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,80 +1,112 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react';
-import { auth } from '../../firebase/firebase.init';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../../firebase/firebase.init";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Register = () => {
-    const [success, setSuccess] = useState(false);
-    const [error, setError] = useState('')
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-    const handleRegister = (event) => {
-        event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.password.value;
-        console.log('register click', email, password)
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log("register click", email, password);
 
-        const length6Pattern = /^.{6,}$/;
-        const casePattern = /^(?=.*[a-z])(?=.*[A-Z]).+$/;
-        const specialCharPattern = /^(?=.*[!@#$%^&*(),.?":{}|<>]).+$/;
+    const length6Pattern = /^.{6,}$/;
+    const casePattern = /^(?=.*[a-z])(?=.*[A-Z]).+$/;
+    const specialCharPattern = /^(?=.*[!@#$%^&*(),.?":{}|<>]).+$/;
 
-        if(!length6Pattern.test(password)){
-            console.log('password didnt match');
-            setError('password must be 6 character or longer');
-            return;
-        }
-        else if (!casePattern.test(password)){
-            setError('password must have at least one uppercase and one lower case character')
-            return;
-        }
-        else if(!specialCharPattern.test(password)){
-            setError('password must contain at least one special character (e.g. ! @ # $ % ^ & *).')
-            return;
-        }
-
-        setError('');
-        setSuccess(false);
-
-        createUserWithEmailAndPassword(auth, email, password)
-        .then(result => {
-            console.log('after creation of a new user',result.user);
-            setSuccess(true);
-            event.target.reset();
-        })
-        .catch(error => {
-            console.log('error happened',error)
-            setError(error.message);
-        })
+    if (!length6Pattern.test(password)) {
+      console.log("password didnt match");
+      setError("password must be 6 character or longer");
+      return;
+    } else if (!casePattern.test(password)) {
+      setError(
+        "password must have at least one uppercase and one lower case character"
+      );
+      return;
+    } else if (!specialCharPattern.test(password)) {
+      setError(
+        "password must contain at least one special character (e.g. ! @ # $ % ^ & *)."
+      );
+      return;
     }
 
-    return (
-        <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Register now!</h1>
-      
-    </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <div className="card-body">
-        <form onSubmit={handleRegister}>
-            <fieldset className="fieldset">
-          <label className="label">Email</label>
-          <input type="email" name='email' className="input" placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" name='password' className="input" placeholder="Password" />
-          <div><a className="link link-hover">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4">Register</button>
-        </fieldset>
-            {
-                success && <p className='text-green-500'>Account created successfully!</p>
-            }
-            {
-                error && <p className='text-red-500'>{error}</p>
-            }
-        </form>
+    setError("");
+    setSuccess(false);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log("after creation of a new user", result.user);
+        setSuccess(true);
+        event.target.reset();
+      })
+      .catch((error) => {
+        console.log("error happened", error);
+        setError(error.message);
+      });
+  };
+
+  const handleTogglePasswordShow = (event) => {
+    event.preventDefault();
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Register now!</h1>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <form onSubmit={handleRegister}>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  className="input"
+                  placeholder="Email"
+                />
+                <label className="label">Password</label>
+                <div className="relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    className="input"
+                    placeholder="Password"
+                  />
+                  <button
+                    onClick={handleTogglePasswordShow}
+                    className="btn btn-xs top-2 right-5 absolute"
+                  >
+                    {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+                  </button>
+                </div>
+                <div>
+                  <label class="label">
+                    <input type="checkbox" class="checkbox" />
+                    Accept Our Terms and Conditions
+                  </label>
+                </div>
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Register</button>
+              </fieldset>
+              {success && (
+                <p className="text-green-500">Account created successfully!</p>
+              )}
+              {error && <p className="text-red-500">{error}</p>}
+            </form>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Register;
